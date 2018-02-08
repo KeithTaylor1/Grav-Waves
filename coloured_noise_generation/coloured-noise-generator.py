f5c7182957300242c7abcff0e6c35bb2b007a479
MNS543 <36199301+MNS543@users.noreply.github.com>
2018-02-08 16:09:26 +0000

    Add files via upload

--- coloured_noise_generation/coloured-noise-generator.py
+++ coloured_noise_generation/coloured-noise-generator.py
@@ -1,15 +1,32 @@
 """
 Created on Tue Feb  6 16:23:46 2018
 
-@author: michael
+@author: michael stephens
 
-Program to take signal in time domain from *.txt file or numpy array and apply 
-noise. Output is a .txt file in time domain. 
+Program to take signal in time domain from *.txt file in same directory and 
+apply noise. Output is a .txt file in time domain. 
+
+Input signal should contain time and signal amplitudes in same file.
+
+Program assumes that the file H1-PSD.txt is in same directory as 
+coloured-noise-generator.py
 """
 
 import numpy as np
+import numpy.fft as fft
+
+filename = input('Enter file name (including \".txt\"): ')
+while 1:
+    try:
+        signal = np.loadtxt(filename)
+        break
+    except FileNotFoundError:
+        print('File not found, please try again... ')
+
+
+
 
 ligo_psd = np.loadtxt('H1-PSD.txt')
+nf = ligo_psd[0, :]
+xf = ligo_psd[1, :]
 
-Pxx = ligo_psd[0,:]
-freq = ligo_psd[1,:]
---
+++ coloured_noise_generation/short_sin_signal.py
@@ -0,0 +1,19 @@
+
+"""
+Created on Thu Feb  8 15:33:37 2018
+
+@author: michael stephens
+
+Creates 3 sec 60Hz sine signal and outputs as *.txt
+
+"""
+
+import numpy as np
+
+
+t = np.arange(0, 3, 0.0001)
+h = np.sin(2*np.pi*60*t)
+
+signal = np.array([t,h])
+
+np.savetxt('signal.txt', signal)
