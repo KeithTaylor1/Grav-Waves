03750e01c7920439a86a26516cbfee5d86d1b92b
njwg1997 <36199311+njwg1997@users.noreply.github.com>
2018-02-09 16:38:50 +0000

    added signal funtionality

--- BlackmanTest.py
+++ BlackmanTest.py
@@ -13,53 +13,109 @@ import matplotlib.mlab as ml
 from scipy import fftpack as fftp
 from scipy import signal
 
-t = np.linspace(0,100,200)
+t = np.linspace(0,100,1000)
 h = sin(2.*pi*t)
-window1 = np.blackman(200)
+window1 = np.blackman(1000)
 '''np.blackman cannot be applied to any 
 data with a different # of points than itself'''
 data1 = h * window1
-data1fft = fftp.fft(window1) / (len(window1)/2.0)
-freq = np.linspace(-0.5, 0.5, len(data1fft))
-response = 20 * np.log10(np.abs(fftp.fftshift(data1fft / abs(data1fft).max())))
+window1fft = fftp.fft(window1) / (len(window1)/2.0)
+freq = np.linspace(-0.5, 0.5, len(window1fft))
+response = 20 * np.log10(np.abs(fftp.fftshift(window1fft / abs(window1fft).max())))
 
 fig1 = pl.figure()
 
-ax1 = fig1.add_subplot(2,1,1)
+ax1 = fig1.add_subplot(3,1,1)
 ax1.plot(window1)
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')
 ax1.grid()
 '''shows spectral leakage of the window in the F domain'''
-ax2 = fig1.add_subplot(2,1,2)
+ax2 = fig1.add_subplot(3,1,2)
 ax2.plot(freq, response)
 ax2.set_xlabel('Frequency')
 ax2.set_ylabel('response')
 ax2.grid()
+
+ax3 = fig1.add_subplot(3,1,3)
+ax3.plot(data1)
+ax3.set_xlabel('X')
+ax3.set_ylabel('Y')
+ax3.grid()
 
 pl.show()
 
 #%%
-window2 = sp.signal.tukey(200)
+window2 = sp.signal.tukey(1000)
 
 data2 = h * window2
 '''sp.signal.tukey cannot be applied to any 
 data with a different # of points than itself'''
-data2fft = fftp.fft(window2,2048) / (len(window2)/2.0)
-freq = np.linspace(-0.5, 0.5, len(data2fft))
-response = 20 * np.log10(np.abs(fftp.fftshift(data2fft / abs(data2fft).max())))
+data2fft = fftp.fft(window2)
+A = data2fft / (len(window2)/2.0)
+freq = np.linspace(-0.5, 0.5, len(A))
+response = 20 * np.log10(np.abs(fftp.fftshift(A / abs(A).max())))
+
 fig2 = pl.figure()
 
-ax1 = fig2.add_subplot(2,1,1)
+ax1 = fig2.add_subplot(3,1,1)
 ax1.plot(window2)
+ax1.set_title('Window plot')
 ax1.set_xlabel('X')
 ax1.set_ylabel('Y')
 ax1.grid()
 '''shows spectral leakage of the window in the F domain'''
-ax2 = fig2 .add_subplot(2,1,2)
+ax2 = fig2 .add_subplot(3,1,2)
 ax2.plot(freq, response)
 ax2.set_xlabel('Frequency')
 ax2.set_ylabel('response')
 ax2.grid()
+
+ax3 = fig2.add_subplot(3,1,3)
+ax3.plot(data2)
+ax3.set_xlabel('X')
+ax3.set_ylabel('Y')
+ax3.grid()
+
+pl.show()
+
+#%%
+
+h = ['noisySinusoid.npy']
+t = ['time.npy']
+DataWindow = sp.signal.tukey(len(h))
+
+data3 = h * DataWindow
+'''sp.signal.tukey cannot be applied to any 
+data with a different # of points than itself'''
+data3fft = fftp.fft(DataWindow)
+A = data3fft / (len(DataWindow)/2.0)
+freq = np.linspace(-0.5, 0.5, len(A))
+response = 20 * np.log10(np.abs(fftp.fftshift(A / abs(A).max())))
+
+fig3 = pl.figure()
+
+ax1 = fig3.add_subplot(3,1,1)
+ax1.plot(h,t)
+ax1.set_title('Window plot')
+ax1.set_xlabel('X')
+ax1.set_ylabel('Y')
+ax1.grid()
+'''shows spectral leakage of the window in the F domain'''
+ax2 = fig3.add_subplot(3,1,2)
+ax2.plot(freq, response)
+ax2.set_xlabel('Frequency')
+ax2.set_ylabel('response')
+ax2.grid()
+
+ax3 = fig3.add_subplot(3,1,3)
+ax3.plot(data3)
+ax3.set_xlabel('X')
+ax3.set_ylabel('Y')
+ax3.grid()
+
+pl.show()
+
+
 
 pl.show()
