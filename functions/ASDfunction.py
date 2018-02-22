ec0a692876a3d59aadd7334d1578343d5f0bcb97
KeithTaylor1 <36199143+KeithTaylor1@users.noreply.github.com>
2018-02-22 15:30:19 +0000

    Update ASDfunction.py

--- functions/ASDfunction.py
+++ functions/ASDfunction.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 16 18:09:18 2018
-
 @author: keithstaylor
 """
 
-def ASDGenerator(ny, d):
+def ASDGenerator(data):
     
     
     import matplotlib.mlab as mlab
@@ -14,31 +13,23 @@ def ASDGenerator(ny, d):
     from scipy import interpolate
     
      
-    t1 = ny[:, 0]
+    t = data[:, 0]
      
-    t2 = d[:, 0]
-    
-    Ts1 = t1[1]-t1[0]
-    Fs1 = 1./Ts1
-    
-    Ts2 = t2[1]-t2[0]
-    Fs2 = 1./Ts2
+    Ts = t[1]-t[0]
+    Fs = 1./Ts
 
-    NFFT1 = 4 * Fs1
+    NFFT = 4 * Fs
     
-    NFFT2 = 4 * Fs2
-    
-    #gaussian noise
-    Pxx_gaussian, freqs = mlab.psd(ny, NFFT = int(NFFT1), Fs = Fs1, window=mlab.window_hanning)
-    PSD_gaussian = interpolate.interp1d(freqs, Pxx_gaussian)
 
-    #coloured noise
-    Pxx_coloured, freqs = mlab.psd(d, NFFT = int(NFFT2), Fs = Fs2, window=mlab.window_hanning)
-    PSD_coloured = interpolate.interp1d(freqs, Pxx_coloured)
     
-    
-    np.savetxt('ASDgaus.txt', np.array([freqs, np.sqrt(Pxx_gaussian)]).T)
-    np.savetxt('ASDcol.txt', np.array([freqs, np.sqrt(Pxx_coloured)]).T)
-    
-    return np.array([freqs, np.sqrt(Pxx_gaussian)]).T, np.array([freqs, np.sqrt(Pxx_coloured)]).T, PSD_gaussian, PSD_coloured 
 
+    Pxx, freqs = mlab.psd(data, NFFT = int(NFFT), Fs = Fs, window=mlab.window_hanning)
+    PSD = interpolate.interp1d(freqs, Pxx)
+
+    
+    
+    
+    np.savetxt('ASDgaus.txt', np.array([freqs, np.sqrt(Pxx)]).T)
+    np.savetxt('ASDcol.txt', np.array([freqs, np.sqrt(Pxx)]).T)
+    
+    return Pxx, PSD
