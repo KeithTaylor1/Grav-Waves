19de8c0a16ea7aa8bfdf8a7cc0b6f3500ddbf094
KeithTaylor1 <36199143+KeithTaylor1@users.noreply.github.com>
2018-02-22 15:43:51 +0000

    Update WindowFunction.py

--- functions/WindowFunction.py
+++ functions/WindowFunction.py
@@ -2,30 +2,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 16 18:09:18 2018
-
 @author: keithstaylor
 """
 
-def Window(gwtd, cwtd):
+def Window(data):
     
     import numpy as np
     from scipy import fftpack as fftp
     import scipy as sp
     
     
-    t = gwtd[:, 0]
+    t = data[:, 0]
     
     Ts = t[1]-t[0]
     Fs = 1./Ts
     
     NFFT = 4 * Fs
     
-    window1 = sp.signal.tukey(NFFT)
-    window1fft = fftp.fft(window1) / (len(window1)/2.0)
-    freq = np.linspace(-0.5, 0.5, len(window1fft))
-    response = 20 * np.log10(np.abs(fftp.fftshift(window1fft / abs(window1fft).max())))
+    window = sp.signal.tukey(NFFT)
+    windowfft = fftp.fft(window) / (len(window)/2.0)
+    freq = np.linspace(-0.5, 0.5, len(windowfft))
+    response = 20 * np.log10(np.abs(fftp.fftshift(windowfft / abs(windowfft).max())))
 
-    data1 = gwtd * window1
-    data2 = cwtd * window1
+    windowed = data * window
     
-    return freq, response, data1, data2 
+    
+    return freq, response, windowed 
